77126b4ca6dcaa574e02d6282ae6af8b185da98c
frankievon <53036832+frankievon@users.noreply.github.com>
2023-11-01 13:28:04 +0300

    Update gp-setwelcome.js

--- plugins/gp-setwelcome.js
+++ plugins/gp-setwelcome.js
@@ -1,10 +1,10 @@
 //import db from '../lib/database.js'
 
-let handler = async (m, { conn, text, isROwner, isOwner }) => {
-  if (text) {
-    global.db.data.chats[m.chat].sWelcome = text
-    m.reply('✅ The welcome message is configured')
-  } else throw `✳️ Enter the Welcome message
+//let handler = async (m, { conn, text, isROwner, isOwner }) => {
+ // if (text) {
+   // global.db.data.chats[m.chat].sWelcome = text
+   // m.reply('✅ The welcome message is configured')
+ // } else throw `✳️ Enter the Welcome message
 }
 handler.help = ['setwelcome <text>']
 handler.tags = ['group']
